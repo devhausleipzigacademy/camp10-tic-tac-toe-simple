e0096eb50fb8f7e4440b9e175becd8617f24d82c
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-06-06 08:55:46 +0200

    forgot to call the function

--- src/main.ts
+++ src/main.ts
@@ -8,22 +8,20 @@ const gridContainer = document.getElementById("gridContainer");
 
 // function generate grid
 
-// function generateGrid(){
-for (let index = 1; index <= 3; index++) {
-	const row = document.createElement("div");
-	row.className = "row";
+function generateGrid() {
+	for (let index = 1; index <= 3; index++) {
+		const row = document.createElement("div");
+		row.className = "row";
 
-	for (let j = 1; j <= 3; j++) {
-		const cell = document.createElement("div");
-		cell.className = "cell";
+		for (let j = 1; j <= 3; j++) {
+			const cell = document.createElement("div");
+			cell.className = "cell";
 
-		row.appendChild(cell);
+			row.appendChild(cell);
+		}
+
+		gridContainer?.appendChild(row);
 	}
-
-	gridContainer?.appendChild(row);
 }
 
-// you need a loop
-// with each loop you creating a div element
-// and for now you appending that div to the dom (appendChild)
-//}
+generateGrid();
