b6dde551f1eef81bdc4d548354db6b5aec8ec4ac
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-06-06 09:36:29 +0200

    cells generated with id and can console.log

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,21 @@ const gridContainer = document.getElementById("gridContainer");
 // function generate grid
 
 function generateGrid() {
-	for (let index = 1; index <= 3; index++) {
+	for (let i = 1; i <= 3; i++) {
 		const row = document.createElement("div");
 		row.className = "row";
 
 		for (let j = 1; j <= 3; j++) {
 			const cell = document.createElement("div");
 			cell.className = "cell";
+
+			// find a way to generate and attach a grid id system for each cell
+			cell.id = `${i}-${j}`;
+
+			// console.log the id of the cell when clicked
+			cell.onclick = function () {
+				console.log(cell.id);
+			};
 
 			row.appendChild(cell);
 		}
