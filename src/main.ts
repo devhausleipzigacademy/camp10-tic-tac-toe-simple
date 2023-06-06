6dd7645a7cb3a867bbf6489f8fadff81d787a65b
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-06-06 08:54:00 +0200

    empty grid container and grid cells no css

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,29 @@
 import "./style.css";
+
+// make a grid-container div in your html
+
+// grab the element from html - selector of some sort (certain type of query *wink wink*)
+
+const gridContainer = document.getElementById("gridContainer");
+
+// function generate grid
+
+// function generateGrid(){
+for (let index = 1; index <= 3; index++) {
+	const row = document.createElement("div");
+	row.className = "row";
+
+	for (let j = 1; j <= 3; j++) {
+		const cell = document.createElement("div");
+		cell.className = "cell";
+
+		row.appendChild(cell);
+	}
+
+	gridContainer?.appendChild(row);
+}
+
+// you need a loop
+// with each loop you creating a div element
+// and for now you appending that div to the dom (appendChild)
+//}
