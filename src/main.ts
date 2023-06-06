9c37304572a707348285d3cbc69cb975085b5b8b
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-06-06 12:20:20 +0200

    tracking gameboard state

--- src/main.ts
+++ src/main.ts
@@ -15,78 +15,78 @@ let scores = { x: 0, o: 0 };
 // boardgame state which at the start of the game is sort of like null
 
 // [
-//     [null, null, null],
-//     [null, null, null],
-//     [null, null, null],
+//     [o, x, 0],
+//     [x, o, x],
+//     [x, o, x],
 // ]
 
 //initialize a gameboard matrix that tracks our x and o
-let gameBoard = [];
+// let gameBoard = [];
 
-for (let i = 0; i < 3; i++) {
-	let row = [];
-	for (let j = 0; j < 3; j++) {
-		row.push(null);
-	}
-	gameBoard.push(row);
-}
+// for (let i = 0; i < 3; i++) {
+// 	let row = [];
+// 	for (let j = 0; j < 3; j++) {
+// 		row.push(null);
+// 	}
+// 	gameBoard.push(row);
+// }
 
-// let gBoard = Array(3)
-// 	.fill(null)
-// 	.map(() => Array(3).fill(null));
+let gameBoard: string[][] = Array(3)
+	.fill(null)
+	.map(() => Array(3).fill(null));
 
 // winning conditions
 const winningConditions = [
 	[
+		[0, 0],
+		[0, 1],
+		[0, 2],
+	],
+	[
+		[1, 0],
 		[1, 1],
 		[1, 2],
-		[1, 3],
 	],
 	[
+		[2, 0],
 		[2, 1],
 		[2, 2],
-		[2, 3],
 	],
 	[
-		[3, 1],
-		[3, 2],
-		[3, 3],
+		[0, 0],
+		[1, 0],
+		[2, 0],
 	],
 	[
+		[0, 1],
 		[1, 1],
 		[2, 1],
-		[3, 1],
 	],
 	[
+		[0, 2],
 		[1, 2],
 		[2, 2],
-		[3, 2],
 	],
 	[
-		[1, 3],
-		[2, 3],
-		[3, 3],
-	],
-	[
+		[0, 0],
 		[1, 1],
 		[2, 2],
-		[3, 3],
 	],
 	[
-		[1, 3],
-		[2, 2],
-		[3, 1],
+		[2, 0],
+		[1, 1],
+		[0, 2],
 	],
 ];
 
 // function generate grid
 
 function generateGrid() {
-	for (let i = 1; i <= 3; i++) {
+	for (let i = 0; i < 3; i++) {
 		const row = document.createElement("div");
 		row.className = "row";
 
-		for (let j = 1; j <= 3; j++) {
+		for (let j = 0; j < 3; j++) {
 			const cell = document.createElement("div");
 			cell.className = "cell";
 
@@ -97,6 +97,10 @@ function generateGrid() {
 			cell.onclick = function () {
 				if (!cell.textContent) {
 					cell.textContent = currentPlayer;
+
+					// set the gameboard with the symbol of the current player
+					gameBoard[i][j] = currentPlayer;
+
 					console.log(cell.id, cell.textContent);
 					currentPlayer = currentPlayer === "x" ? "o" : "x";
 					player1?.classList.toggle("active");
