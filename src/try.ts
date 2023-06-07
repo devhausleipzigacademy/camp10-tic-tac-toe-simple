bfc4aba354d7f20a71f127e385c298432a5c5209
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-06-07 08:55:34 +0200

    fixed bugs, game is working

--- src/try.ts
+++ src/try.ts
@@ -1,7 +1,5 @@
 import "./style.css";
 
-// grab the elements from the dom
-
 const gridContainer = document.getElementById("gridContainer");
 const player1 = document.getElementById("player1");
 const player2 = document.getElementById("player2");
@@ -10,14 +8,6 @@ const player2 = document.getElementById("player2");
 let currentPlayer = "x";
 
 let scores: { [key: string]: number } = { x: 0, o: 0 };
-
-// boardgame state which at the start of the game is sort of like null: this is where we will track our x and o
-
-// [
-//     [o, x, 0],
-//     [x, o, x],
-//     [x, o, x],
-// ]
 
 let gameBoard: string[][] = Array(3)
 	.fill(null)
@@ -70,6 +60,8 @@ const winningConditions = [
 // function generate grid
 
 function generateGrid() {
+	// Remove children
+
 	while (gridContainer?.firstChild) {
 		gridContainer.removeChild(gridContainer.firstChild);
 	}
@@ -82,10 +74,8 @@ function generateGrid() {
 			const cell = document.createElement("div");
 			cell.className = "cell";
 
-			// attach id to cells
 			cell.id = `${i}-${j}`;
 
-			// on click handler for the cell
 			cell.onclick = function () {
 				if (!cell.textContent && !checkWin()) {
 					cell.textContent = currentPlayer;
@@ -93,10 +83,7 @@ function generateGrid() {
 					// set the gameboard with the symbol of the current player
 					gameBoard[i][j] = currentPlayer;
 
-					if (checkWin()) {
-						return; // If the current player has won, stop here
-					}
-
+					console.log(gameBoard);
 					currentPlayer = currentPlayer === "x" ? "o" : "x";
 					player1?.classList.toggle("active");
 					player2?.classList.toggle("active");
@@ -111,49 +98,10 @@ function generateGrid() {
 }
 
 function checkWin() {
-	const hasWon = winningConditions.some((condition) => {
+	return winningConditions.some((condition) => {
 		const allCellMarkedByCurrentPlayer = condition.every(([row, col]) => {
 			return gameBoard[row][col] === currentPlayer;
 		});
 		return allCellMarkedByCurrentPlayer;
 	});
-
-	if (hasWon) {
-		updateScoreAndDisplayWinner();
-	}
-
-	return hasWon;
 }
-
-function resetGame() {
-	gameBoard = Array(3)
-		.fill(null)
-		.map(() => Array(3).fill(null));
-	currentPlayer = "x";
-	generateGrid();
-}
-
-function updateScoreAndDisplayWinner() {
-	setTimeout(() => {
-		scores[currentPlayer]++;
-		alert(`Player ${currentPlayer.toUpperCase()} won the game!`);
-
-		// Update player scores in the DOM
-		if (currentPlayer === "x") {
-			const scoreElement = player1?.querySelector(".score");
-			if (scoreElement) {
-				scoreElement.textContent = scores[currentPlayer].toString();
-			}
-		} else {
-			const scoreElement = player2?.querySelector(".score");
-			if (scoreElement) {
-				scoreElement.textContent = scores[currentPlayer].toString();
-			}
-		}
-
-		// Reset the game
-		resetGame();
-	}, 100);
-}
-
-generateGrid();
